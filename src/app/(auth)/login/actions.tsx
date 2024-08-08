35c93ab95ce1cfdb06517426757ef65148dd1abf
fadlystwn <fadlystwn@gmail.com>
2024-08-08 18:06:37 +0700

    fix login and validation issue

--- src/app/(auth)/login/actions.tsx
+++ src/app/(auth)/login/actions.tsx
@@ -1,44 +1,52 @@
 'use server';
+import { z } from 'zod'
 import prisma from '@/lib/db';
 import { handleSesion } from '@/lib/auth';
-import { compare } from 'bcrypt'
+import { compare } from 'bcrypt';
+import { redirect } from 'next/navigation';
 
-export default async function login(formData: FormData) {
+const schema = z.object({
+  email: z.string({
+    invalid_type_error: 'Invalid Email',
+  }),
+})
+
+export default async function login(prevState: any, formData: FormData) {
+
+  const validatedFields = schema.safeParse({
+    email: formData.get('email'),
+  })
 
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
   try {
     const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
+      where: { email },
     });
 
-    if (!user) {
+    if (!validatedFields.success) {
       return {
-        message: 'User not found!',
-      };
+        errors: validatedFields.error.flatten().fieldErrors,
+      }
+    }
+
+    if (!user) {
+      return { message: 'User not found!' };
     }
 
     const passwordMatch = await compare(password, user.password);
     if (!passwordMatch) {
-      return {
-        message: 'Invalid password!',
-      };
+      return { message: 'Invalid password!' };
     }
 
-    const sessionData = {
-      userId: user.id,
-      email: user.email,
-    };
-
+    const sessionData = { userId: user.id, email: user.email };
     await handleSesion(sessionData);
 
-  } catch (err) {
-    console.error('Database Error:', err);
-    return {
-      message: 'Database Error: Failed to Find User.',
-    };
+  } catch (error) {
+    console.error('Database Error:', error);
+    return { message: 'Database Error: Failed to Find User.' };
   }
+
+  redirect('/news')
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,40 +1,49 @@
-"use client"
+"use client";
+
 import { redirect } from 'next/navigation';
 import login from './actions';
 import styles from './login.module.css';
+import { useFormState } from 'react-dom';
 
-const LoginPage = () => {
+const initialState = {
+  message: ''
+}
 
-  async function handleAction(formData: FormData) {
-    //validation here
-    await login(formData)
-    redirect('/news')
-  }
+export default function LoginPage() {
+
+  const [state, formAction] = useFormState(login, initialState)
+  console.log(state)
 
   return (
     <div className={styles.container}>
-      <form className={styles.form} action={handleAction}>
+      <form className={styles.form} action={formAction}>
         <h2>Login</h2>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          className={styles.input}
-          required
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          className={styles.input}
-          required
-        />
+        <div className={styles.inputGroup}>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            className={styles.input}
+            required
+          />
+
+        </div>
+        <div className={styles.inputGroup}>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className={styles.input}
+            required
+          />
+        </div>
+        <p aria-live="polite" className="error-field sr-only">
+          {state?.message}
+        </p>
         <button type="submit" className={styles.button}>
           Login
         </button>
       </form>
     </div>
   );
-};
-
-export default LoginPage
+}
