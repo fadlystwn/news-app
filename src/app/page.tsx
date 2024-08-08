55e7b780d3a89936536ce079bad07b709efecdb0
fadlystwn <fadlystwn@gmail.com>
2024-08-08 18:12:15 +0700

    add static pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>HELLO</div>
-    </main>
+    <div>HELLO</div>
   );
 }
