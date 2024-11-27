bbb960b1e27f4f56a0735619e35250a6ea325bf1
fadlydev <fadlystwn@gmail.com>
2024-11-27 08:03:38 +0700

    fix interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,13 @@
 export interface User {
+  id: number;
   name: string;
   email: string;
   phone: string | null;
+  password: string;
   Profile: Profile | null;
-
 }
 
-export interface Profile extends User {
+export interface Profile {
   id: number;
   bio: string | null;
   avatarUrl: string | null;
